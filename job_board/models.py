b49868c3f704b3a3b12c297ad345d6c2320d305e
Alijon Karimberdiev <coding802@gmail.com>
2023-08-19 15:51:42 -0400

    First Commit

---
+++ job_board/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company= models.CharField(max_length=100)
+    salary = models.IntegerField()
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} | {self.company} | Active: {self.is_active}"
+    
+
+
+#makemigrations
+## -> creates instructions telling django how the db changed
+#migrate
+## -> applies the above changes
+
+
